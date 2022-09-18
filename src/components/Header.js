8f38979e5090a34539fd116ec8e22d980c498a19
diego-salves <diegosantos57@outlook.com.br>
2022-09-18 19:42:31 -0300

    added products to main page

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { Navbar, Nav, Container, Row } from 'react-bootstrap';
 function Header() {
   return (
     <header>
-            <Navbar bg="dark" variant="dark" expand="lg" collapśeOnSelect>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect >
               <Container>
                 <Navbar.Brand href="/">ProShop</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
