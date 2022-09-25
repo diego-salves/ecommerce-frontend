153ad16e8147b3288518bc688134a2ce3fa93386
diego-salves <diegosantos57@outlook.com.br>
2022-09-25 19:06:59 -0300

    New: Created ProductScreen Structure

--- src/products.js
+++ src/products.js
@@ -17,11 +17,11 @@ const products = [
     'name': 'iPhone 11 Pro 256GB Memory',
     'image': '/images/phone.jpg',
     'description':
-      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life.',
     'brand': 'Apple',
     'category': 'Electronics',
     'price': 599.99,
-    'countInStock': 7,
+    'countInStock': 0,
     'rating': 4.0,
     'numReviews': 8,
   },
